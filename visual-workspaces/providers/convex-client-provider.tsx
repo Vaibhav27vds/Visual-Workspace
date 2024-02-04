62e4957b700707f2ee992bcf8da523ca3e1f280d
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-02-05 03:32:39 +0530

    convex client provider file completed

--- visual-workspaces/providers/convex-client-provider.tsx
+++ visual-workspaces/providers/convex-client-provider.tsx
@@ -1,3 +1,27 @@
 "use client";
 
-import 
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+
+interface ConvexClientProviderProps {
+    children: React.ReactNode;
+};
+
+const converxUrl  = process.env.NEXT_PUBLIC_CONVERX_URL!;
+
+const convex = new ConvexReactClient(converxUrl);
+
+export const  ConvexClientProvider = ({
+    children, 
+}:  ConvexClientProviderProps) => {
+    return (
+        <ClerkProvider>
+            <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+                {children}
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
+
+    );
+};
+
