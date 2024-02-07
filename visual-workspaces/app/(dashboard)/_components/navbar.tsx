c3c03006e56f581cc4d29d47055c9538a3565486
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-02-08 04:41:50 +0530

    Navbar Completed

--- visual-workspaces/app/(dashboard)/_components/navbar.tsx
+++ visual-workspaces/app/(dashboard)/_components/navbar.tsx
@@ -1,13 +1,44 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, OrganizationSwitcher, useOrganization } from "@clerk/nextjs";
+import { SearchInput } from "./search-input";
+import { InviteButton } from "./invite-button";
 
 export const Navbar = () => {
+    const { organization } = useOrganization();
     return(
-        <div className="flex items-center gap-x-4 p-5 bg-green-500">
-            <div className="hidden lg:flex lg:flex-1 bg-yellow-500">
-                Search 
+        <div className="flex items-center gap-x-4 p-5">
+            <div className="hidden lg:flex lg:flex-1">
+                <SearchInput  />
             </div>
+            <div className="block lg:hidden flex-1">
+            <OrganizationSwitcher 
+            hidePersonal
+            appearance={{
+                elements: {
+                    rootBox:{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        width: "100%",
+                        maxWidth: "376px",
+                    },
+                    organizationSwitcherTrigger: {
+                        padding: "6px",
+                        width:"100%",
+                        borderRadius: "8px",
+                        border:  "1px solid #E5E7EB",
+                        justifyContent: "space-between",
+                        backgroundColor: "white",
+                    }
+                }
+            }}
+            
+            />
+            </div>
+            {organization && (
+            <InviteButton />
+            )}
             <UserButton />
             </div>
     );
