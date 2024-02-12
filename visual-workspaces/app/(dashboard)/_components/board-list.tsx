d1906857862b5c22470c0ecda27ac92e96279d1c
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-02-13 04:17:40 +0530

    Board List created

--- visual-workspaces/app/(dashboard)/_components/board-list.tsx
+++ visual-workspaces/app/(dashboard)/_components/board-list.tsx
@@ -1,8 +1,14 @@
 "use client"
 
+
+import { useQuery } from "convex/react";
+
+import { api } from "@/convex/_generated/api";
 import { EmptyBoards } from "./empty-boards";
 import { EmptyFavourites } from "./empty-favourites";
 import { EmptySearch } from "./empty-search";
+import { BoardCard } from "./board-card";
+import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
     orgId: string;
@@ -17,14 +23,37 @@ export const BoardList = ({
     query,
 }: BoardListProps) => {
 
-    const data = []; // TODO: Change to api call
+    const data = useQuery(api.boards.get, { orgId });
+
+    if(data === undefined){
+        return(
+            <div>
+            <h2 className="text-3xl">
+                {query.favourites ? "Favourite boards" : "Team boards"}
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+
+                <NewBoardButton  orgId={orgId} disabled />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                </div>
+                </div>
+        )
+    }
 
     if(!data?.length && query.search){
         return(
             <div>
                 <EmptySearch />
             </div>
-        );
+        )
     }
 
     if(!data?.length && query.favourites){
@@ -38,7 +67,28 @@ export const BoardList = ({
 
     return(
         <div>
-            {JSON.stringify(query)}
+            <h2 className="text-3xl">
+                {query.favourites ? "Favourite boards" : "Team boards"}
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+
+                <NewBoardButton orgId={orgId} />
+
+                {data?.map((board) => (
+                    <BoardCard  
+                    key={board._id}
+                    id={board._id}
+                    title={board.title}
+                    imageUrl={board.imageUrl}
+                    authorId={board.authorId}
+                    authorName={board.authorId}
+                    createdAt={board._creationTime}
+                    orgId={board.orgId}
+                    isFavourite={false}                   
+                    />
+                ))}
+
+            </div>
         </div>
     );
 
