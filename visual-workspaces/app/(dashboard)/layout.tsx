489a96c07cf5ae403ddfded6267641eb70cc7531
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-02-13 04:33:36 +0530

    Double screen issue resolved

--- visual-workspaces/app/(dashboard)/layout.tsx
+++ visual-workspaces/app/(dashboard)/layout.tsx
@@ -11,18 +11,17 @@ const DashboardLayout = ({
 }:  DashboardLayoutProps)  => {
     return (
         <main className="h-full">
-            <Sidebar />
-            <div className="pl-[60px] h-full">
-                <div  className="flex gap-x-3 h-full">
-                    <OrgSidebar />
-                    <div className="h-full flex-1">
-                        <Navbar />
-                    {children}
-                    </div>
-                </div>
-            {children}
-            </div >
-        </main>
+        <Sidebar />
+        <div className="pl-[60px] h-full">
+          <div className="flex gap-x-3 h-full">
+            <OrgSidebar />
+            <div className="h-full flex-1">
+              <Navbar />
+              {children}
+            </div>
+          </div>
+        </div>
+      </main>
     );
 };
 
