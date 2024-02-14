51776c40f6abecae05d398f8834741d0a496bedb
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-02-15 05:20:06 +0530

    updated layout and index

--- visual-workspaces/app/(dashboard)/_components/board-card/index.tsx
+++ visual-workspaces/app/(dashboard)/_components/board-card/index.tsx
@@ -8,7 +8,8 @@ import { useAuth } from "@clerk/nextjs";
 import { create } from "domain";
 import { Footer } from "./footer";
 import { Skeleton } from "@/components/ui/skeleton";
-
+import { Actions } from "@/components/actions";
+import { MoreHorizontal } from "lucide-react";
 
 interface BoardCardProps {
     id: string;
@@ -49,6 +50,19 @@ export const BoardCard = ({
                     className="object-fit"
                     />
                     <Overlay />
+                    <Actions
+                    id={id}
+                    title={title}
+                    side="right"
+                    >
+                        <button
+                        className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacitypx-3 py-2 outline-none"
+                        >
+                            <MoreHorizontal  
+                            className="text-white opacity-75 hover:opacity-100 transition-opacity"
+                            />
+                        </button>
+                        </Actions>
                 </div>
                 <Footer 
                 isFavourite={isFavourite}
