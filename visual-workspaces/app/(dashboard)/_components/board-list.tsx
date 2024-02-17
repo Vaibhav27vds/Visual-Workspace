67c2d9c08b021ebd52fb00b8a2597335327a5eb8
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-02-18 02:45:45 +0530

    board updated with favorites

--- visual-workspaces/app/(dashboard)/_components/board-list.tsx
+++ visual-workspaces/app/(dashboard)/_components/board-list.tsx
@@ -1,95 +1,82 @@
-"use client"
-
+"use client";
 
 import { useQuery } from "convex/react";
 
 import { api } from "@/convex/_generated/api";
-import { EmptyBoards } from "./empty-boards";
-import { EmptyFavourites } from "./empty-favourites";
-import { EmptySearch } from "./empty-search";
+
 import { BoardCard } from "./board-card";
+import { EmptySearch } from "./empty-search";
+import { EmptyBoards } from "./empty-boards";
+import { EmptyFavorites } from "./empty-favorites";
 import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
-    orgId: string;
-    query: {
-        search?: string;
-        favourites?: string;
-    };
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
 };
 
 export const BoardList = ({
-    orgId,
-    query,
+  orgId,
+  query,
 }: BoardListProps) => {
+  const data = useQuery(api.boards.get, { 
+    orgId,
+    ...query,
+  });
 
-    const data = useQuery(api.boards.get, { orgId });
-
-    if(data === undefined){
-        return(
-            <div>
-            <h2 className="text-3xl">
-                {query.favourites ? "Favourite boards" : "Team boards"}
-            </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
-
-                <NewBoardButton  orgId={orgId} disabled />
-                <BoardCard.Skeleton />
-                <BoardCard.Skeleton />
-                <BoardCard.Skeleton />
-                <BoardCard.Skeleton />
-                <BoardCard.Skeleton />
-                <BoardCard.Skeleton />
-                <BoardCard.Skeleton />
-                <BoardCard.Skeleton />
-                <BoardCard.Skeleton />
-                </div>
-                </div>
-        )
-    }
-
-    if(!data?.length && query.search){
-        return(
-            <div>
-                <EmptySearch />
-            </div>
-        )
-    }
-
-    if(!data?.length && query.favourites){
-        return <EmptyFavourites />
-    }
-
-    if(!data?.length){
-        return <EmptyBoards />
-    }
-
-
-    return(
-        <div>
-            <h2 className="text-3xl">
-                {query.favourites ? "Favourite boards" : "Team boards"}
-            </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
-
-                <NewBoardButton orgId={orgId} />
-
-                {data?.map((board) => (
-                    <BoardCard  
-                    key={board._id}
-                    id={board._id}
-                    title={board.title}
-                    imageUrl={board.imageUrl}
-                    authorId={board.authorId}
-                    authorName={board.authorId}
-                    createdAt={board._creationTime}
-                    orgId={board.orgId}
-                    isFavourite={false}                   
-                    />
-                ))}
-
-            </div>
+  if (data === undefined) {
+    return (
+      <div>
+        <h2 className="text-3xl">
+          {query.favorites ? "Favorite boards" : "Team boards"}
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+          <NewBoardButton orgId={orgId} disabled />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
+          <BoardCard.Skeleton />
         </div>
-    );
+      </div>
+    )
+  }
 
+  if (!data?.length && query.search) {
+    return <EmptySearch />;
+  }
+
+  if (!data?.length && query.favorites) {
+    return <EmptyFavorites />
+  }
+
+  if (!data?.length) {
+    return <EmptyBoards />
+  }
+
+  return (
+    <div>
+      <h2 className="text-3xl">
+        {query.favorites ? "Favorite boards" : "Team boards"}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+        <NewBoardButton orgId={orgId} />
+        {data?.map((board) => (
+          <BoardCard
+            key={board._id}
+            id={board._id}
+            title={board.title}
+            imageUrl={board.imageUrl}
+            authorId={board.authorId}
+            authorName={board.authorName}
+            createdAt={board._creationTime}
+            orgId={board.orgId}
+            isFavorite={board.isFavorite}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
--- visual-workspaces/convex/board.ts
+++ visual-workspaces/convex/board.ts
@@ -55,7 +55,20 @@ export const remove = mutation({
             throw new Error ("Unauthorized");
         }
 
-        //  TODO: Later check to delete favorite relation as well
+        const userId = identity.subject
+
+        const existingFavorite = await ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_board", (q) => 
+        q
+          .eq("userId", userId)
+          .eq("boardId", args.id)
+        )
+        .unique();
+
+        if(existingFavorite){
+            await ctx.db.delete(existingFavorite._id);
+        }
 
         await ctx.db.delete(args.id);
     }
@@ -86,6 +99,83 @@ export const update = mutation({
 
         return board;
 
+    },
+});
+
+export const favorite = mutation({
+    args: { id: v.id("boards"), orgId: v.string() },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const board = await ctx.db.get(args.id);
+
+        if(!board) {
+            throw new Error("Board not found");
+        }
+
+        const userId = identity.subject;
+
+        const existingFavorite = await ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_board", (q) => 
+        q
+        .eq("userId", userId)
+        .eq("boardId", board._id)
+        ) 
+        .unique();
+
+        if(existingFavorite){
+            throw new Error("Board already favorited");
+        }
+
+        await ctx.db.insert("userFavorites", {
+            userId,
+            boardId: board._id,
+            orgId: args.orgId,
+                });
+
+                return board;
+    },
+});
+
+
+export const unfavorite = mutation({
+    args: { id: v.id("boards")},
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if(!identity) {
+            throw new Error("Unauthorized");
+        }
+
+        const board = await ctx.db.get(args.id);
+
+        if(!board) {
+            throw new Error("Board not found");
+        }
+
+        const userId = identity.subject;
+
+        const existingFavorite = await ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_board", (q) => 
+        q
+        .eq("userId", userId)
+        .eq("boardId", board._id)
+        ) 
+        .unique();
+
+        if(!existingFavorite){
+            throw new Error("Favorited board not found");
+        }
+
+        await ctx.db.delete(existingFavorite._id)
+
+                return board;
     },
 });
 
