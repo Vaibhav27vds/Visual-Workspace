37f5e60b73b60c4b6b646cee6f703808a1b53fbe
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-02-18 21:31:50 +0530

    the avatars are added to the participants

--- visual-workspaces/app/board/[boardId]/_components/participants.tsx
+++ visual-workspaces/app/board/[boardId]/_components/participants.tsx
@@ -1,14 +1,59 @@
+"use client";
+
+import { UserAvatar } from "./user-avatar";
+import { useOther, useOthers, useSelf } from "@/liveblocks.config";
+
 import { Skeleton } from "@/components/ui/skeleton";
+import { connectionIdToColor } from "@/lib/utils";
+
+const MAX_SHOWN_USERS = 2;
+
+
 
 export const Participants = () => {
+
+    const users = useOthers();
+    const currentUser = useSelf();
+    const hasMoreUsers = users.length > MAX_SHOWN_USERS;
     return (
         <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-            Lists Of users
+            <div className="flex gap-x-2">
+                {users.slice(0, MAX_SHOWN_USERS).map(({ connectionId, info }) => {
+                    return (
+                        <UserAvatar 
+                        borderColor={connectionIdToColor(connectionId)}
+                        key={connectionId}
+                        src={info?.picture}
+                        name={info?.name}
+                        fallback={info?.name?.[0] || "T"}
+                        />
+                    )
+                })}
+
+                {currentUser && (
+                    <UserAvatar 
+                    borderColor={connectionIdToColor(currentUser.connectionId)}
+                    src={currentUser.info?.picture}
+                    name={`${currentUser.info?.name} (You)`}
+                    fallback={currentUser.info?.name?.[0]} 
+                    
+                    />
+                )}
+
+                {hasMoreUsers && (
+                    <UserAvatar 
+                    name={`${users.length - MAX_SHOWN_USERS} more`}
+                    fallback={`+${users.length - MAX_SHOWN_USERS}`}
+
+                    />
+                )}
+                
+            </div>
         </div>
     );
 };
 
-Participants.Skeleton = function ParticipantsSkeleton() {
+export const ParticipantsSkeleton = () => {
     return(
         <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md w-[100px]">
             <Skeleton className="h-full w-full bg-muted-400"/>
