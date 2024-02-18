37b630dc308193fab8ad6454e933cba1d16e0ce5
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-02-18 21:31:30 +0530

    The loading section is updated

--- visual-workspaces/app/board/[boardId]/_components/loading.tsx
+++ visual-workspaces/app/board/[boardId]/_components/loading.tsx
@@ -1,8 +1,8 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import { Loader } from "lucide-react";
-import { Info } from "./info";
-import { Participants } from "./participants";
-import { Toolbar } from "./toolbar";
+import { Info, InfoSkeleton } from "./info";
+import { Participants, ParticipantsSkeleton } from "./participants";
+import { Toolbar, ToolbarSkeleton } from "./toolbar";
 
 export const Loading = () => {
     return (
@@ -10,9 +10,9 @@ export const Loading = () => {
 
             <Loader className="h-6 w-6 text-muted-foreground animate-spin"/>
 
-            <Info.Skeleton />
-            <Participants.Skeleton />
-            <Toolbar.Skeleton />
+            <InfoSkeleton />
+            <ParticipantsSkeleton />
+            <ToolbarSkeleton />
 
         </main>
     );
