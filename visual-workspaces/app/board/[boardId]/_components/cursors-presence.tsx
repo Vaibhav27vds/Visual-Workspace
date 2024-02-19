a68fbe320336ebdab2c85a2e08a8d0bb53e1db79
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-02-20 04:51:26 +0530

    added cursor presence feature

---
+++ visual-workspaces/app/board/[boardId]/_components/cursors-presence.tsx
@@ -0,0 +1,31 @@
+"use client";;
+
+import { memo } from "react";
+import { useOthersConnectionIds } from "@/liveblocks.config";
+import { Cursor } from "./cursor";
+
+const Cursors = () =>{
+    const ids = useOthersConnectionIds();
+
+    return (
+        <>
+        {ids.map((connectionId) => (
+            <Cursor 
+            key={connectionId}
+            connectionId={connectionId}
+            />
+        ))}
+        </>
+    )
+}
+
+export const CursorsPresence = () => {
+    return(
+        <>
+        {/* TODO: Draft pencil */}
+          <Cursors />
+        </>
+    );
+};
+
+CursorsPresence.displayName = "CursorsPresence";
