d4543d9f834bece11b0d27e1969d9600b38dd7ab
Vaibhav27vds <Vaibhav27vds@gmail.com>
2024-02-20 04:53:42 +0530

    updated the room.tsx

--- visual-workspaces/components/room.tsx
+++ visual-workspaces/components/room.tsx
@@ -2,7 +2,8 @@
 
 import { ReactNode } from "react";
 import { ClientSideSuspense } from "@liveblocks/react";
-
+import { LiveMap, LiveList, LiveObject } from "@liveblocks/client";
+import { Layer } from "@/types/canvas";
 import { RoomProvider } from "@/liveblocks.config";
 
 interface RoomProps {
@@ -20,7 +21,11 @@ export const Room = ({
 }: RoomProps) => {
 
     return(
-        <RoomProvider id={roomId} initialPresence={{}}>
+        <RoomProvider id={roomId} initialPresence={{cursor: null, selection: [],}}
+        initialStorage={{
+            layers: new LiveMap<string, LiveObject<Layer>>(),
+            layerIds: new LiveList(),
+        }}>
             <ClientSideSuspense fallback ={fallback}>
                 {() => children}
             </ClientSideSuspense>
